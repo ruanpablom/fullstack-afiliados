fb9f5b97b240366e65740bce7583fec2e9bc049a
Ruan Medeiros <pm.ruan@gmail.com>
2023-08-07 15:13:45 -0300

    feat: upload form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  UploadFormValues,
+  uploadFormSchema,
+} from './schemas/upload-form-schema';
+import { Input } from './components/Input';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(): JSX.Element {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UploadFormValues>({
+    resolver: zodResolver(uploadFormSchema),
+  });
+
+  const onSubmit = (data: UploadFormValues) => {
+    console.info(data.file[0]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      id="container"
+      className="flex flex-col gap-4 p-6 bg-slate-900 w-96 rounded-lg border border-emerald-400"
+    >
+      <h1 className="text-lg font-bold text-emerald-400">
+        Upload arquivo de transações
+      </h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          id="file"
+          label="Arquivo:"
+          type="file"
+          multiple={false}
+          {...register('file')}
+          errors={errors.file}
+        />
+        <input
+          className="mr-auto bg-emerald-400 text-slate-900 font-semibold px-2 py-1 rounded-sm cursor-pointer hover:bg-emerald-300"
+          type="submit"
+          value="Enviar"
+        />
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
